470611018b6fca9c847588923d6dcf19b0caf887
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2022-08-22 10:38:27 +0200

    first commit

---
+++ src/ios/Safe.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+#import <Security/SecRandom.h>
+#import <Cordova/CDV.h>
+#import "RNEncryptor.h"
+#import "RNDecryptor.h"
+
+
+@interface Safe : CDVPlugin {
+}
+
+- (void)encrypt:(CDVInvokedUrlCommand*)command;
+- (void)decrypt:(CDVInvokedUrlCommand*)command;
+- (NSString*)crypto:(NSString*)action command:(CDVInvokedUrlCommand*)command;
+@end
